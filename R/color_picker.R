8bdfe642449ecf3ba2b802a2bba0d2bfdc44dccf
Shinya Uryu <suika1127@gmail.com>
2015-01-14 19:47:48 +0900

    [fix]🐛

--- R/color_picker.R
+++ R/color_picker.R
@@ -1,17 +1,17 @@
 #' color picker using in file
 #' 
+#' @import magrittr
 #' @export
 color_picker <- function(file, show = TRUE) {
-  d <- readLines(file)
-  
-  d <- grep("#", d, value = TRUE) %>%
+  d <- readLines(file) %>%
+    grep("#", ., value = TRUE) %>%
     strsplit(split = " +") %>%
-    unlist()
-
-  d <- grep("#[[:alnum:]]", d, value = TRUE) %>%
+    unlist() %>%
+    grep("#[[:alnum:]]", ., value = TRUE) %>%
     strsplit(split = " +") %>%
     unlist() %>%
     stringr::str_extract("#[[:alnum:]]+") %>%
+    grep("^#[[:alnum:]]{6}", .,  value = TRUE) %>%
     unique() %>%
     print()
   
